1711425d45a3d0cf634cff63800d3e84889da810
SiddhantJain <siddhantjain027@gmail.com>
2024-06-22 03:36:08 +0530

    implement home page ui-chart/welcome

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalance from '@/components/TotalBalance';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: "Siddhant"};
   return (
-    <div>
-      
-    </div>
+    <section className="home"> 
+     <div className="home-content">
+        <header className="home-header">
+            <HeaderBox
+             type="greeting"
+             title="Welcome"
+             user={loggedIn?.firstName || 'Guest'}
+             subtext="Access and manage your account and transactions efficiently"
+            />
+
+            <TotalBalance
+              accounts={[]}
+              totalBanks={1} 
+              totalCurrentBalance={1250.35}/>
+
+
+        </header>
+     </div>
+    </section>
   )
 }
 
